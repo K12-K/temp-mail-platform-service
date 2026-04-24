4fc6c34b3b4714bf6718ef99b909f7e458271cbe
K12-K <kalp12122000@gmail.com>
2026-04-24 17:12:35 +0530

    updated redis connection flow

--- services/redis.js
+++ services/redis.js
@@ -1,6 +1,13 @@
 // services/redis.js
 import Redis from "ioredis";
 
-const redis = new Redis(process.env.REDIS_URL);
+// const redis = new Redis(process.env.REDIS_URL);
+const redis = new Redis({
+    host: process.env.REDIS_HOST,
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD,
+    maxRetriesPerRequest: null,
+    port: process.env.REDIS_PORT,
+});
 
 export default redis;
