3411141b12525bf04789f4db65eb0bc459a217c7
K12-K <kalp12122000@gmail.com>
2026-06-18 11:58:51 +0530

    updated email generation api flow to handle persistent storage on refresh and expiry timer

--- app.js
+++ app.js
@@ -31,5 +31,5 @@ app.use("/api/inbox", inboxRoutes);
 app.use("/webhook", webhookRoutes);
 
 server.listen(process.env.PORT || 8000, () => {
-  console.log("Server running on port 3000");
+  console.log(`Server running on port ${process.env.PORT || 8000}`);
 });
